68610fa530141bdc168c484e238b3f2955baa57b
yizhou <yi@123.com>
2021-05-20 15:55:11 +0800

    'third'

--- src/views/scheme/components/white/index.jsx
+++ src/views/scheme/components/white/index.jsx
@@ -1,7 +1,79 @@
 import React, { Component, createRef } from 'react'
-import { Form, Switch, Button, Card } from 'antd'
-import { InfoCircleOutlined } from '@ant-design/icons';
+import { Form, Switch, Button, Card, Space, Input, Table, Tag } from 'antd'
+import { InfoCircleOutlined, SearchOutlined } from '@ant-design/icons';
 import './index.scss'
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+        render: text => <a>{text}</a>,
+    },
+    {
+        title: 'Age',
+        dataIndex: 'age',
+        key: 'age',
+    },
+    {
+        title: 'Address',
+        dataIndex: 'address',
+        key: 'address',
+    },
+    {
+        title: 'Tags',
+        key: 'tags',
+        dataIndex: 'tags',
+        render: tags => (
+            <>
+                {tags.map(tag => {
+                    let color = tag.length > 5 ? 'geekblue' : 'green';
+                    if (tag === 'loser') {
+                        color = 'volcano';
+                    }
+                    return (
+                        <Tag color={color} key={tag}>
+                            {tag.toUpperCase()}
+                        </Tag>
+                    );
+                })}
+            </>
+        ),
+    },
+    {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+            <Space size="middle">
+                <a>Invite {record.name}</a>
+                <a>Delete</a>
+            </Space>
+        ),
+    },
+];
+
+const data = [
+    {
+        key: '1',
+        name: 'John Brown',
+        age: 32,
+        address: 'New York No. 1 Lake Park',
+        tags: ['nice', 'developer'],
+    },
+    {
+        key: '2',
+        name: 'Jim Green',
+        age: 42,
+        address: 'London No. 1 Lake Park',
+        tags: ['loser'],
+    },
+    {
+        key: '3',
+        name: 'Joe Black',
+        age: 32,
+        address: 'Sidney No. 1 Lake Park',
+        tags: ['cool', 'teacher'],
+    },
+];
 export default class index extends Component {
     formRef = createRef();
     onFinish = (value) => {
@@ -36,10 +108,14 @@ export default class index extends Component {
                 <Form.Item style={{ marginTop: 10 }}>
                     <Card>
                         <div>
-                            <Button>新增白名单成员</Button>
-                            <Button>清空全部白名单</Button>
-                            <Button>踢人话术设置</Button>
+                            <Space>
+                                <Button type="primary">新增白名单成员</Button>
+                                <Button>清空全部白名单</Button>
+                                <Button>踢人话术设置</Button>
+                            </Space>
                         </div>
+                        <Input style={{ width: 226, marginTop: 10 }} placeholder="搜索成员" suffix={<SearchOutlined />}></Input>
+                        <Table columns={columns} dataSource={data} />
                     </Card>
                 </Form.Item>
             </Form>
